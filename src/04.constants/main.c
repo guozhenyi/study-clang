ac588a7a5f3fcd69ad95bb1d3fa27e657c836fd9
gzy <gzyonline@hotmail.com>
2023-08-09 18:04:15 +0800

    constants

---
+++ src/04.constants/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//
+#define AGE 22
+
+int main()
+{
+  const int LENGTH = 10;
+  const int WIDTH = 5;
+  const char NEWLINE = '\n';
+  int area = LENGTH * WIDTH;
+
+  printf("value of area: %d", area);
+  printf("%c", NEWLINE);
+
+  return 0;
+}
+
